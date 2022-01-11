d089ebf20edb9f9314754057d60ab508a2cbbacd
Ketan <76541886+km322@users.noreply.github.com>
2022-01-10 20:02:44 -0800

    Update 2021-2022- VexCode.cpp

--- 2021-2022- VexCode.cpp
+++ 2021-2022- VexCode.cpp
@@ -20,8 +20,8 @@ using namespace vex;
 competition Competition;
 
 //Back Lift
-vex::motor  BackRLift = vex::motor( vex:: PORT4);
-vex::motor  BackLLift = vex::motor( vex:: PORT3,true);
+vex::motor  BackRLift = vex::motor( vex:: PORT4,true);
+vex::motor  BackLLift = vex::motor( vex:: PORT3);
 
 //Front Lift
 vex::motor  FrontRLift = vex::motor( vex:: PORT2,true);
