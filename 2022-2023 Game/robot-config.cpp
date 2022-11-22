b2198bb5d48168b53071ad23e924cc740f4db23f
Ketan <76541886+km322@users.noreply.github.com>
2022-11-22 12:52:53 -0800

    Update robot-config.cpp

--- 2022-2023 Game/robot-config.cpp
+++ 2022-2023 Game/robot-config.cpp
@@ -1,1 +1,27 @@
+#include "vex.h"
 
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+/*vex-vision-config:begin*/
+signature Vision11__DISK = signature (1, -1233, -613, -923, -4719, -4061, -4390, 11, 0);
+vision Vision11 = vision (PORT11, 50, Vision11__DISK);
+/*vex-vision-config:end*/
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
